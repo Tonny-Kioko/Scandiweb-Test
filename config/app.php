b83b43a897754b0193e29bbf41f788b2d63b48a8
Tonny-Kioko <anthonykioko5525@gmail.com>
2022-08-11 16:26:45 +0300

    Junior developer test

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 # Database Configuration
-define('DB_HOST', 'mysql');
-define('DB_PORT', '3306');
-define('DB_NAME', 'scandiweb_junior_test');
-define('DB_USER', 'test');
-define('DB_PASS', 'test');
+define('DB_HOST', 'ec2-44-195-162-77.compute-1.amazonaws.com');
+define('DB_PORT', '5432');
+define('DB_NAME', 'dam3530vktd0ng');
+define('DB_USER', 'wrcdowhupmxozp');
+define('DB_PASS', '4d5f0b57d8a59a8237823834b89b7705b22b35a99e4ed4232161815fd43376d7');
